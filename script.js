451eaa202be8ce16cb5058481991c8117a27eb32
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2021-11-09 21:06:52 +0200

    Make javaScript that add data dynamically

--- script.js
+++ script.js
@@ -12,3 +12,125 @@ navLinks.addEventListener('click', (event) => {
     }
   }
 });
+
+
+const data = [
+  {
+    id:1,
+    projectPopName: "Tonic",
+    techPoptype: "CANOPY",
+    techPopDevType: "Back End Dev",
+    techPopDevYear: "2015",
+    ProjectPopImageUrl: "/images/Snapshoot-Portfolio.png",
+    ProjectPopDesc: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    usedLang1: "html",
+    usedLang2: "CSS",
+    usedLang3: "JavaScript",
+  },
+  {
+    id:2,
+    projectPopName: "Multi-Post Stories",
+    techPoptype: "FACEBOOK",
+    techPopDevType: "Full Stack Dev",
+    techPopDevYear: "2015",
+    ProjectPopImageUrl: "./images/Snapshoot-Portfolio1.png",
+    ProjectPopDesc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    usedLang1: "html",
+    usedLang2: "CSS",
+    usedLang3: "JavaScript",
+  },
+  {
+    id:3,
+    projectPopName: "Facebook 360",
+    techPoptype: "FACEBOOK",
+    techPopDevType: "Full Stack Dev",
+    techPopDevYear: "2015",
+    ProjectPopImageUrl: "./images/Snapshoot-Portfolio2.png",
+    ProjectPopDesc: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    usedLang1: "html",
+    usedLang2: "CSS",
+    usedLang3: "JavaScript",
+  },
+  {
+    id:4,
+    projectPopName: "Uber Navigation",
+    techPoptype: "Uber",
+    techPopDevType: "Lead Developer",
+    techPopDevYear: "2018",
+    ProjectPopImageUrl: "./images/Snapshoot-Portfolio3.png",
+    ProjectPopDesc: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    usedLang1: "html",
+    usedLang2: "CSS",
+    usedLang3: "JavaScript",
+  }
+];
+
+const progectPopData = data[0];
+
+const projectPop = document.createElement("div");
+projectPop.setAttribute("class", "project-pop");
+projectPop.setAttribute("id", "project-pop");
+  const projectPopContainer = document.createElement("div");
+  projectPopContainer.setAttribute("class", "project-pop-container");
+    const projectPopName = document.createElement("h2");
+    projectPopName.setAttribute("class", "project-pop-name");
+    projectPopName.innerHTML = progectPopData.projectPopName;
+    const techPop = document.createElement("div");
+    techPop.setAttribute("class", "tech-pop");
+      const techPopType = document.createElement("p");
+      techPopType.innerHTML = progectPopData.techPoptype
+      const techPopSpan1 = document.createElement("span");
+      techPopSpan1.innerHTML = "•"
+      const techPopDevType = document.createElement("p");
+      techPopDevType.innerHTML = progectPopData.techPopDevType
+      const techPopSpan2 = document.createElement("span");
+      techPopSpan2.innerHTML = "•"
+      const techPopDevYear = document.createElement("p");
+      techPopDevYear.innerHTML = progectPopData.techPopDevYear
+    const ProjectPopImage = document.createElement("img");
+    ProjectPopImage.setAttribute("src", progectPopData.ProjectPopImageUrl)
+    ProjectPopImage.setAttribute("alt", "project image")
+    const ProjectPopDesc = document.createElement("p");
+    ProjectPopDesc.innerHTML = progectPopData.ProjectPopDesc
+    const usedLang = document.createElement("div");
+    usedLang.setAttribute("class", "used-lang");
+      const usedLangP1 = document.createElement("p");
+      usedLangP1.innerHTML = progectPopData.usedLang1
+      const usedLangP2 = document.createElement("p");
+      usedLangP2.innerHTML = progectPopData.usedLang2
+      const usedLangP3 = document.createElement("p");
+      usedLangP3.innerHTML = progectPopData.usedLang3
+    const popBtns = document.createElement("div");
+    popBtns.setAttribute("class", "pop-btn");
+      const liveBtn = document.createElement("button");
+      liveBtn.setAttribute("class", "live-btn");
+      liveBtn.setAttribute("type", "button");
+      liveBtn.innerHTML = 'See live <i class="fas fa-arrow-circle-right"></i>'
+      const sourceBtn = document.createElement("button");
+      sourceBtn.setAttribute("class", "live-btn");
+      sourceBtn.setAttribute("type", "button");
+      sourceBtn.innerHTML = 'See Source <i class="fab fa-github"></i>'
+
+
+const body = document.body;
+
+
+techPop.append(techPopType);
+techPop.append(techPopSpan1);
+techPop.append(techPopDevType);
+techPop.append(techPopSpan2);
+techPop.append(techPopDevYear);
+usedLang.append(usedLangP1);
+usedLang.append(usedLangP2);
+usedLang.append(usedLangP3);
+popBtns.append(liveBtn);
+popBtns.append(sourceBtn);
+projectPopContainer.append(projectPopName);
+projectPopContainer.append(techPop);
+projectPopContainer.append(ProjectPopImage);
+projectPopContainer.append(ProjectPopDesc);
+projectPopContainer.append(usedLang);
+projectPopContainer.append(popBtns);
+projectPop.append(projectPopContainer);
+console.log(body,projectPop)
+body.append(projectPop);
